bd5d59f2fe45b4c5c399b2dae44ddb0a6f5e2825
shawofomaha <markshaw.us@gmail.com>
2017-05-16 00:29:21 -0500

    jqury

---
+++ WordPress-Class/php-folder/index.php
@@ -0,0 +1,13 @@
+<style>
+h1
+</style>
+<h1>Marko Dude</h1>
+<!--<?php echo date("l"); ?>-->
+<?php echo date("G:ia l"); ?>
+<?php date_default_timezone_set('America/Chicago'); ?>
+<!--<?php phpinfo(); ?>-->
+ <?php $a = "long time ago"; $b = 3.14; $c = true; ?>
+
+<?php var_dump($a, $b, $c  ); ?>
+
+<p> It is <?php echo date('z'); ?>th day of the month</p>
